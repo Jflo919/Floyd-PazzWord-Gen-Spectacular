13ebc2b734e3e6ab14267ba7c854f32c3fa6070d
JulianF <floydjulian1991@gmail.com>
2022-02-16 09:02:20 -0800

    added a rather long block of psuedocode at the top, created the first prompt window, no code to capture input yet, but it took me a moment to realize to name the function generatePassword to match the startercode, added return statement at bottom of the mostly empty generatePassword() with placeholder text, clicking the button in the app now displays the placeholder text. Created 4 different arrays for special, numbers, lower, and upper characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,21 @@
 // 
 
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["!", "@", "?", "#", "%", "^", "&", "*", "~" ];
+
+var generatePassword = function() {
+  console.log("You clicked the button");
+  
+  // ask user for the length of the desired password,must be >= 8 characters and <= 128 characters.
+  window.prompt("Please enter the desired character length(between 8 and 128 characters) of your password to be generated.");
+
+
+// placeholder for returned generated password here, later will be actual generated password.
+  return "generated password";
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
